42f4b72881569c0929ef0d59eec3f8c44ed933f8
JanBoeker <naj_b@gmx.de>
2020-10-15 22:50:21 +0200

    add bmi calculator

--- calculator.js
+++ calculator.js
@@ -1,11 +1,31 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/bmicalculator", function(req, res){
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/", function(req, res){
+  var firstNumber = req.body.num1;
+  var secondNumber = req.body.num2;
+  var result = Number(firstNumber) + Number(secondNumber);
+  res.send(firstNumber + " + " + secondNumber + " = " + result);
+});
+
+app.post("/bmicalculator", function(req, res){
+  var weight = Number(req.body.weight);
+  var hight = Number(req.body.hight);
+  var bmi = Math.round(weight / (hight * hight));
+  res.send("Your BMI is " + bmi + ".");
 });
 
 app.listen(3000, function(){
